07ab36354a91029a6c0ca873ecac5ba4cff77f38
Andyrei02 <acenusa58@gmail.com>
2023-03-02 22:48:18 +0200

    repair bug data folder

--- utils/config.py
+++ utils/config.py
@@ -15,6 +15,8 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 if TELEGRAM_TOKEN is None:
 	raise ValueError("Telegram API token not found in environment variables")
 
+if not os.path.exists('data'): os.mkdir('data')
+
 USER_DATABASE_NAME = "data.db"
 USER_DATABASE_URL = f"data/{USER_DATABASE_NAME}"
 
